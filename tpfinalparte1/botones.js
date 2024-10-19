6c2d4a6fa00260cd82413a2a6e3b65d381b0c699
Guillermo Joaquín Franco <103509525+Warframes@users.noreply.github.com>
2024-10-18 21:38:11 -0300

    Add files via upload

---
+++ tpfinalparte1/botones.js
@@ -0,0 +1,15 @@
+function botonesDeAvance() {
+    if (estado == 4 ||  estado == 9 ||  estado == 17 || estado ==21) {
+    fill(255, 0, 0);
+    rect(110, 335, 100, 50, 50);
+    fill(0, 0, 255);
+    rect(430, 335, 100, 50, 50);
+    noStroke();
+    push();
+     textSize(30);
+     fill(255);
+     text("Si",145, 370 );
+     text("No", 465, 370);
+     pop();
+  }
+}
