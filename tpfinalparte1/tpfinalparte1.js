afc3a4d1625c40a3ca09a559ed54f62cd968f5ad
Guillermo Joaquín Franco <103509525+Warframes@users.noreply.github.com>
2024-10-19 21:56:09 -0300

    Update tpfinalparte1.js

--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,16 +1,12 @@
-let pantallas = [24];
+let pantallas = [23];
 let estado = 0;
 let cancionBase;
 let font;
-let anchoPantalla = 640;
-let altoPantalla = 480;
-let anchoTexto;
-let tamTexto;
-let inicioTexto;
-let posTextoActual = 0;
+let misTextos;
+
 
 function preload() {
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < 23; i++) {
     pantallas[i] = loadImage('data/' + i + '.png');
   }
   font = loadFont('data/angrybirds-regular.ttf')
@@ -30,63 +26,58 @@ function draw() {
   background(0);
   console.log(mouseX, mouseY, estado);
   image(pantallas[estado], 0, 0, width, height);
+  textosDePantallas();
   botonReinicio();
   botonSiguiente();
   botonesDecisivos();
-  push();
-  fill(250);
-  textSize(20);
-  text(misTextos[obtenerTextActual()], inicioTexto, inicioTexto, anchoTexto);
-  reload();
-  pop();
+  if (estado == 23) {
+    creditos();
+  }
 }
 function mousePressed() {
-  if(estado == 0){
-  clickBoton();
+  if (estado == 0) {
+    if (mouseX > 360 && mouseX < 460 && mouseY > 400 && mouseY < 450) {
+      estado++;
+    }
   }
-
   if ( estado == 1 ||  estado == 2 ||  estado == 3
     ||  estado == 6 ||  estado == 7 ||  estado == 8 ||  estado == 10
-    ||  estado == 11 ||  estado == 12 ||  estado == 13 ||  estado == 15
-    ||  estado == 16 ||  estado == 18 || estado == 20 || estado == 23) {
+    ||  estado == 11 ||  estado == 12 || estado == 14 ||  estado == 15
+    ||  estado == 18 || estado == 20  || estado == 21|| estado == 22) {
     clickBoton();
-  } else if (estado == 4 ||  estado == 9 ||  estado == 17 || estado ==21) {
+  } else if (estado == 4 ||  estado == 9 ||  estado == 16 || estado == 19) {
     if (mouseX>110&&mouseX<210&&mouseY>335&&mouseY<385) {
 
       if (estado == 4 ) {
         estado = 6;
       } else if ( estado == 9) {
-        estado = 15;
-      } else if ( estado == 17) {
-        estado = 20;
-      } else if ( estado == 21) {
-        estado = 22;
+        estado = 14;
+      } else if ( estado == 16) {
+        estado = 17;
+      } else if ( estado == 19) {
+        estado = 21;
       }
     } else if (mouseX>430&&mouseX<530&&mouseY>335&&mouseY<385) {
-      if (estado ==4) {
-        estado = 5;
-      } else if ( estado == 9) {
-        estado = 10;
-      } else if ( estado == 17) {
+      if (estado==16) {
         estado = 18;
-      } else if ( estado == 21) {
-        estado = 23;
+      } else if  (estado == 19) {
+        estado = 22;
+      } else {
+        estado++;
       }
     }
   } else if (estado == 5) {
     estado = 7;
   }
+ 
+
+  if (estado == 23) {
+    if (mouseX>290&&mouseX<390&&mouseY>400&&mouseY<450) {
+      estado = 0;
+    }
+  }
 }
 
-function reload() {
-
-
-  anchoTexto = anchoPantalla * 0.8;
-  tamTexto = altoPantalla * 0.10;
-  inicioTexto = anchoPantalla * 0.05;
-
-  textSize(tamTexto);
-}
 
 
 function keyPressed() {
