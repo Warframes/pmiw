92940635f20360b200c2d17d95bc1efea83c5a16
Guillermo Joaquín Franco <103509525+Warframes@users.noreply.github.com>
2024-10-19 21:56:29 -0300

    Update funciones.js

--- tpfinalparte1/funciones.js
+++ tpfinalparte1/funciones.js
@@ -1,10 +1,23 @@
-function obtenerTextActual() {
-  return posTextoActual;
+function textosDePantallas() {
+  if (estado < misTextos.length) {
+    fill(255);
+    textSize(15);
+    text(misTextos[estado], 50, 180);
+  }
 }
 function clickBoton() {
   if (mouseX > 270 && mouseX < 370 && mouseY > 400 && mouseY < 450) {
     estado++;
-    posTextoActual++;
   }
 }
-//rect(270, 400, 100, 50, 50);
+function creditos () {
+  image(pantallas[23], 0, 0, width, height);
+  fill(255, 0, 0);
+  textAlign(CENTER);
+  textSize(30);
+  text ("Créditos", width/2, 220);
+  textSize(20);
+  text("Julieta Pratti legajo 83917/3", width/2, height/2+10);
+  text("Guillermo Franco legajo 91308/0", width/2, height/2+50 );
+  text("Comision 5 - Trabajo Práctico Final Parte 1", width/2, height/2+90 );
+}
